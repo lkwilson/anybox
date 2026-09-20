41f5c52de0aacc82879f79419e22f346fecbf13d
Lucas Wilson <lkwilson96@gmail.com>
2026-09-20 12:02:28 -0600

    fix search

---
+++ skel/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true,
+            ignored = false,
+          },
+          files = {
+            hidden = true,
+            ignored = false,
+          },
+          grep = {
+            hidden = true,
+            ignored = false,
+          },
+        },
+      },
+    },
+  },
+}
